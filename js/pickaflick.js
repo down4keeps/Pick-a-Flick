6eeb13fad8a41dfd2e64a1a9dd2f6cf48101ef6f
Jamie Piazza <piazza.jamie@gmail.com>
2015-06-28 11:45:35 -0700

    Updated for RT API + Star Ratings (not working)
    attempted adding the movie titles into the names of the stars to no
    avail…

--- js/pickaflick.js
+++ js/pickaflick.js
@@ -1,23 +1,39 @@
 $(document).ready(function(){
 
+
+
 $.getJSON( "http://127.0.0.1:5000/neohackerz", function( data ) {
   var movies = [];
   $.each( data, function( key, val ) {
     movies.push(
+// HAD TO ADD '\' TO THE END FOR LINE BREAKS!
     	'<div class="col-md-4"> \
-	  	<h2>' + val.show_title + '</h2> \
-	  	<img src="' + val.poster + '" /> \
-	  	<p>' + val.summary + '</p> \
-		</div>'
+	  	<h2>' + val.title + '</h2> \
+	  	<img src="' + val.posters.original.replace(/^.*?\/[\d]+x[\d]+\//,"http://") + '" /> \
+      <h5 style="display: block;">Your Rating:</h5> \
+      <fieldset class="score"> \
+      <legend>Score:</legend> \
+      <input type="radio" id="score-5" name="score-' + val.title + '" value="5"/> \
+      <label title="5 stars" for="score-5">5 stars</label> \
+      <input type="radio" id="score-4" name="score-' + val.title + '" value="4"/> \
+      <label title="4 stars" for="score-4">4 stars</label> \
+      <input type="radio" id="score-3" name="score-' + val.title + '" value="3"/> \
+      <label title="3 stars" for="score-3">3 stars</label> \
+      <input type="radio" id="score-2" name="score-' + val.title + '" value="2"/> \
+      <label title="2 stars" for="score-2">2 stars</label> \
+      <input type="radio" id="score-1" name="score-' + val.title + '" value="1"/> \
+      <label title="1 stars" for="score-1">1 stars</label> \
+      </fieldset> \
+	  	<p class="synopsis">' + val.synopsis + '</p> \
+      </div>'
 	);
-
     console.log(val);
   });
  
   $( "<div>", {
     "class": "my-new-movie-list",
     html: movies.join( "" )
-  }).appendTo( "body" );
+  }).insertBefore( ".row" );
 });
 
 });
